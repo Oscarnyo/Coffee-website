ed37f2411dc091607ab4b6abdf9795ef0f9034e0
nyo oscar <oscarnyo728@gmail.com>
2022-07-12 22:51:08 +0800

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 /*=============== LOADER ===============*/
-
+onload = () =>{
+    const load = document.getElementById('load')
+    
+    setTimeout(() =>{
+        load.style.display = 'none'
+    },1600)
+}
 
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
